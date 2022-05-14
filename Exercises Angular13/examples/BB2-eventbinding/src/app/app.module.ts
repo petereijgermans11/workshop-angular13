f5a1908153eaed75f8c856f154062146701afafd
Peter Eijgermans <p.eijgermans11@gmail.com>
2022-05-14 07:19:00 +0200

    update browserslistrc file

--- Exercises Angular13/examples/BB2-eventbinding/src/app/app.module.ts
+++ Exercises Angular13/examples/BB2-eventbinding/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+//import { AppComponent } from './app.component';
 import {AppComponent2} from './app-02-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //AppComponent2
+    //AppComponent,
+    AppComponent2
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent2]
 })
-export class AppModule { }
+export class AppModule {
+}
